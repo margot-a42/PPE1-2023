352aa321091b385ce723e06dece0201732d9603c
margot-a42 <75041813+margot-a42@users.noreply.github.com>
2023-10-23 22:45:52 +0200

    Add files via upload

--- exercices/compte_par_annee.sh
+++ exercices/compte_par_annee.sh
@@ -11,4 +11,14 @@ annees=$(echo "$annees" | cut -c 1-4)
 comptage_annees=$(echo "$annees" | sort | uniq -c)
 
 echo "Nombre de fichiers ou dossiers par année :"
-echo "$comptage_annees"
+if [ -n $comptage_annees]
+then 
+	echo "le premier argument n'est pas vide"
+	if [[ $comptage_annees =~bon(jour|soir)]]
+	then 
+		echo "salut"
+	fi
+else
+	echo "le premier argument est vide"
+fi
+echo "fin du programme"
